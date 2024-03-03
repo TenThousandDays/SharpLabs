b2207ee45c1891f2b11b2c63998aae12440fabc2
solar <solar@KOMPIK>
2024-03-03 22:10:31 +0700

    Made a class which reads'n'yields numbers from file until EOF

---
+++ FileSort/Program.cs
@@ -0,0 +1,64 @@
+﻿
+file class FileIntReader
+{
+    private FileStream fs;
+    private byte[] curr;
+    private List<byte> buffer;
+
+    public FileIntReader(string filename)
+    {
+        fs = new FileStream(filename, FileMode.Open);
+        curr = new byte[1];
+        buffer = new List<byte>();
+    }
+
+    public int NextInt()
+    {
+        int num;
+        while (fs.Read(curr, 0, 1) != 0)
+        {
+            if (curr[0] != 0x20)
+            {
+                buffer.Add(curr[0]);
+            }
+            else
+            {
+                if (!int.TryParse(buffer.ToArray(), out num))
+                {
+                    throw new Exception("Error while reading numbers from file!");
+                }
+                curr[0] = 0x00;
+                buffer.Clear();
+                return num;
+            }
+        }
+        if (int.TryParse(buffer.ToArray(), out num))
+        {
+            curr[0] = 0x00;
+            buffer.Clear();
+            return num;
+        }
+        fs.Close();
+        return -1;
+    }
+}
+class Program
+{
+    public static void FileSort(string path)
+    {
+        return;
+    }
+
+    public static void Main()
+    {
+        string fn = @"D:\SharpLabs\SharpLabs\FileSort\1.txt";
+        //FileSort(fn);
+        FileIntReader fir = new FileIntReader(fn);
+        int n = fir.NextInt();
+        while(n >= 0)
+        {
+            Console.WriteLine($"Read from file: {n}");
+            n = fir.NextInt();
+        }
+    }
+}
