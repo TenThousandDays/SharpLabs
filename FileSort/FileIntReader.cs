95ac4dad42d384f8bbb24ebdb34fa770a6e063fb
solar <solar@KOMPIK>
2024-03-04 22:58:18 +0700

    File Merge-sorting done

--- FileSort/FileIntReader.cs
+++ FileSort/FileIntReader.cs
@@ -3,24 +3,24 @@
     class FileIntReader
     {
         private FileStream fs;
-        private byte[] curr;
+        private byte[] cur;
         private List<byte> buffer;
 
         public FileIntReader(string filename)
         {
             fs = new FileStream(filename, FileMode.Open);
-            curr = new byte[1];
+            cur = new byte[1];
             buffer = new List<byte>();
         }
 
         public int NextInt()
         {
             int num;
-            while (fs.Read(curr, 0, 1) != 0)
+            while (fs.Read(cur, 0, 1) != 0)
             {
-                if (curr[0] != 0x20)
+                if (cur[0] != 0x20)
                 {
-                    buffer.Add(curr[0]);
+                    buffer.Add(cur[0]);
                 }
                 else
                 {
@@ -28,19 +28,24 @@
                     {
                         throw new Exception("Error while reading numbers from file!");
                     }
-                    curr[0] = 0x00;
+                    cur[0] = 0x00;
                     buffer.Clear();
                     return num;
                 }
             }
             if (int.TryParse(buffer.ToArray(), out num))
             {
-                curr[0] = 0x00;
+                cur[0] = 0x00;
                 buffer.Clear();
                 return num;
             }
-            fs.Close();
+            Close();
             return -1;
+        }
+
+        public void Close()
+        {
+            fs.Close();
         }
     }
 }
--- FileSort/Program.cs
+++ FileSort/Program.cs
@@ -10,13 +10,11 @@ namespace FileSort
         {
             return Encoding.UTF8.GetBytes($"{x} ");
         }
-        public static void SplitFile(string filename)
+        public static void SplitFile(string filename, string out_fn_1, string out_fn_2)
         {
-            string first_fn = filename + ".A";
-            string second_fn = filename + ".B";
             FileStream[] F = new FileStream[2];
-            F[0] = new FileStream(first_fn, FileMode.Create);
-            F[1] = new FileStream(second_fn, FileMode.Create);
+            F[0] = new FileStream(out_fn_1, FileMode.Create);
+            F[1] = new FileStream(out_fn_2, FileMode.Create);
 
             FileIntReader fir = new FileIntReader(filename);
             int x1 = fir.NextInt();
@@ -35,54 +33,117 @@ namespace FileSort
 
             F[0].Close();
             F[1].Close();
+            fir.Close();
             return;
         }
-        private static int[] Merge(int[] a, int[] b)
-        {
-            int i = 0;
-            int j = 0;
-            int k = 0;
 
-            int n = a.Length;
-            int m = b.Length;
-            int[] result = new int[n + m];
-            while (i < n && j < m)
+        public static void MergeFiles(string in_file_1, string in_file_2, string out_file_1, string out_file_2)
+        {
+            FileIntReader[] S = new FileIntReader[2];
+            S[0] = new FileIntReader(in_file_1);
+            S[1] = new FileIntReader(in_file_2);
+            
+            FileStream[] F = new FileStream[2];
+            F[0] = new FileStream(out_file_1, FileMode.Create);
+            F[1] = new FileStream(out_file_2, FileMode.Create);
+
+            int[] x = new int[2];
+            int[] y = new int[2];
+            int n, m;
+            n = m = 0;
+
+            x[0] = S[0].NextInt();
+            x[1] = S[1].NextInt();
+            while (x[0] >= 0 && x[1] >= 0)
             {
-                if (a[i] < b[j])
+                if (x[m] > x[1 - m])
                 {
-                    result[k++] = a[i++];
+                    m = 1 - m;
                 }
-                else
+                F[n].Write(make_bytes(x[m]));
+                y[m] = S[m].NextInt();
+                if (y[m] < 0 || x[m] > y[m])
                 {
-                    result[k++] = b[j++];
+                    m = 1 - m;
+                    F[n].Write(make_bytes(x[m]));
+                    y[m] = S[m].NextInt();
+                    while (y[m] >= 0 && x[m] <= y[m])
+                    {
+                        x[m] = y[m];
+                        F[n].Write(make_bytes(x[m]));
+                        y[m] = S[m].NextInt();
+                    }
+                    x[1 - m] = y[1 - m];
+                    n = 1 - n;
                 }
+                x[m] = y[m];
             }
-            if (i < n)
+
+            // !!!
+            while (y[0] >= 0)
             {
-                while (i < n)
+                F[n].Write(make_bytes(x[0]));
+                y[0] = S[0].NextInt();
+                if (x[0] > y[0])
                 {
-                    result[k++] = a[i++];
+                    n = 1 - n;
                 }
+                x[0] = y[0];
             }
-            else
+            while (y[1] >= 0)
             {
-                while (j < m)
+                F[n].Write(make_bytes(x[1]));
+                y[1] = S[1].NextInt();
+                if (x[1] > y[1])
                 {
-                    result[k++] = b[j++];
+                    n = 1 - n;
                 }
+                x[1] = y[1];
             }
-            return result;
+            F[0].Close();
+            F[1].Close();
+            S[0].Close();
+            S[1].Close();
         }
+
         public static void FileSort(string path)
         {
-            SplitFile(path);
+            string f_1 = path + ".A";
+            string f_2 = path + ".B";
+            string s_1 = f_1 + ".A";
+            string s_2 = f_2 + ".B";
+            List<string> files = [f_1, f_2, s_1, s_2];
+
+            try
+            {
+                SplitFile(path, f_1, f_2);
+                long file_len;
+                while (true)
+                {
+                    file_len = new FileInfo(f_1).Length;
+                    if (file_len == 0)
+                    {
+                        return;
+                    }
+                    MergeFiles(f_1, f_2, s_1, s_2);
+                    file_len = new FileInfo(s_1).Length;
+                    if (file_len == 0)
+                    {
+                        return;
+                    }
+                    MergeFiles(s_1, s_2, f_1, f_2);
+                }
+            }
+            catch (ObjectDisposedException)
+            {
+                return;
+            }
         }
 
         public static void Main()
         {
             string fn = @"D:\SharpLabs\SharpLabs\FileSort\1.txt";
             FileSort(fn);
-            
         }
     }
 }
